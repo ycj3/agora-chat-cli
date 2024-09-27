d7c73169adb9ddfc461c16f2964033a554da7616
CarlsonYuan <carlsonyuandev@gmail.com>
2024-09-27 18:35:33 +0800

    add support fcm(v1), apns(p8), support apns fields for fcm

--- cmd/push_provider.go
+++ cmd/push_provider.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 	ac "github.com/ycj3/agora-chat-cli/agora-chat"
 	"github.com/ycj3/agora-chat-cli/util"
@@ -34,165 +35,325 @@ var listProvidersCmd = &cobra.Command{
 	},
 }
 
-var deleteProviderCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "delete the provider",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		uuid, _ := cmd.Flags().GetString("uuid")
+type deleteOptions struct {
+	Uuids []string
+}
 
-		res, err := client.Provider().DeletePushProvider(uuid)
-		if err != nil {
-			return fmt.Errorf("failed to delete the provider: %w", err)
-		}
+func deleteProviderCmd() *cobra.Command {
+	opts := &deleteOptions{}
 
-		msg := fmt.Sprintf("Push provider [%s] was deleted successfully", res.Entities[len(res.Entities)-1].Name)
-		logger.Info(msg, nil)
-		return nil
-	},
+	var cmd = &cobra.Command{
+		Use:   "delete",
+		Short: "delete the provider",
+		Example: heredoc.Doc(`
+				$ agchat push provider delete --uuid <provider-uuid1>,<provider-uuid2>
+		`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			for _, uuid := range opts.Uuids {
+				res, err := client.Provider().DeletePushProvider(uuid)
+				if err != nil {
+					return fmt.Errorf("failed to delete the provider: %w", err)
+				}
+
+				logger.Info("Deleted push provider", map[string]interface{}{
+					"name": res.Entities[0].Name,
+				})
+			}
+			return nil
+		},
+	}
+
+	fl := cmd.Flags()
+	fl.StringSliceVarP(&opts.Uuids, "uuid", "u", nil, "Uuids of the push provider")
+
+	return cmd
 }
 
 // APNS P8
-var apnsCmd = &cobra.Command{
-	Use:   "insert-apns",
-	Short: "Insert an APNS push provider",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		apnsTeamId, _ := cmd.Flags().GetString("team-id")
-		apnsKeyId, _ := cmd.Flags().GetString("key-id")
+// var apnsCmd = &cobra.Command{
+// 	Use:   "insert-apns",
+// 	Short: "Insert an APNS push provider",
+// 	RunE: func(cmd *cobra.Command, args []string) error {
+// 		apnsTeamId, _ := cmd.Flags().GetString("team-id")
+// 		apnsKeyId, _ := cmd.Flags().GetString("key-id")
 
-		apns := getPushProviderFromCommonFlags(cmd)
+// 		apns := getPushProviderFromCommonFlags(cmd)
 
-		apns.Type = ac.PushProviderAPNS
-		apns.ApnsPushSettings = &ac.APNSConfig{
-			TeamId: apnsTeamId,
-			KeyId:  apnsKeyId,
-		}
+// 		apns.Type = ac.PushProviderAPNS
+// 		apns.ApnsPushSettings = &ac.APNSConfig{
+// 			TeamId: apnsTeamId,
+// 			KeyId:  apnsKeyId,
+// 		}
 
-		res, err := client.Provider().InsertPushProvider(apns)
-		if err != nil {
-			logger.Error("failed to send request,", map[string]interface{}{
-				"error": err,
-			})
-			return nil
-		}
-		logger.Info("Push provider inserted successfully", map[string]interface{}{
-			"results": res.Entities,
-		})
-		return nil
-	},
+// 		res, err := client.Provider().InsertPushProvider(apns)
+// 		if err != nil {
+// 			logger.Error("failed to send request,", map[string]interface{}{
+// 				"error": err,
+// 			})
+// 			return nil
+// 		}
+// 		logger.Info("Push provider inserted successfully", map[string]interface{}{
+// 			"results": res.Entities,
+// 		})
+// 		return nil
+// 	},
+// }
+
+type upsertOption struct {
+
+	// PushProvide type
+	IsAPNs   bool
+	IsFCM    bool
+	IsHuawei bool
+
+	NotifierId string // Same as UUID, it is used when editing the notifier.
+
+	IsEnvDev bool
+
+	Name        string
+	Environment string
+	Certificate string
+	PackageName string
+	Sound       string
+
+	CertificateFilePath string
+
+	// *** FCM ***
+	// push message type
+	IsBoth         bool
+	IsNotifacation bool
+	IsData         bool
+
+	// Push Priority
+	IsPriorityHigh   bool
+	IsPriorityNormal bool
+
+	SupportAPNs bool
+
+	// *** APNs ***
+	TeamID string
+	KeyID  string
 }
 
-var insertFcmCmd = &cobra.Command{
-	Use:   "insert-fcm",
-	Short: "Insert an FCM push provider",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fcmPushType, _ := cmd.Flags().GetString("push-type")
-		fcmPriority, _ := cmd.Flags().GetString("priority")
-		fcmProjectId, _ := cmd.Flags().GetString("project-id")
-		fcmVersion, _ := cmd.Flags().GetString("version")
+func upsertFcmCmd() *cobra.Command {
+	opts := &upsertOption{}
 
-		fcm := getPushProviderFromCommonFlags(cmd)
-		fcm.Type = ac.PushProviderFCM
-		fcm.FcmPushSettings = &ac.FCMConfig{
-			PushType:  fcmPushType,
-			Priority:  fcmPriority,
-			ProjectId: fcmProjectId,
-			Version:   fcmVersion,
-		}
+	var cmd = &cobra.Command{
+		Use:   "upsert",
+		Short: "Inserts a push provider if it doesn't exist, or updates the existing push provider if it does",
+		Example: heredoc.Doc(`
 
-		res, err := client.Provider().InsertPushProvider(fcm)
-		if err != nil {
-			logger.Error("failed to send request,", map[string]interface{}{
-				"error": err,
+				# Insert a APNs (P8) push provider
+				$ agchat push provider upsert --apns --name <certificate-name> --package-name <bundle-id> --dev-env --key-id <key-id> --team-id <team-id> --file <apns-p8-file>
+
+				# Insert a FCM (V1) push provider
+				$ agchat push provider upsert --fcm --name <fcm-sender-id> --file <fcm-service-account-json-file-path> --high --both
+
+				# Enable APNs specific fields (e.g. sound, mutable-content)
+				$ agchat push provider upsert --fcm --support-apns --nofitier-id <notifitier-id>
+
+				# Disable APNs specific fields (e.g. sound, mutable-content)
+				$ agchat push provider upsert --fcm --support-apns=false --nofitier-id <notifitier-id>
+		`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// var providerType ac.PushProviderType
+			var env ac.EnvironmentType
+			var provider ac.PushProvider
+
+			if !opts.IsEnvDev {
+				env = ac.EnvProduct
+			} else {
+				env = ac.EnvDevelopment
+			}
+
+			if opts.IsFCM {
+				var pushType string
+				var priority string
+
+				if opts.IsNotifacation {
+					pushType = ac.FCMPushNotification
+				} else if opts.IsData {
+					pushType = ac.FCMPushData
+				} else {
+					pushType = ac.FCMPushBoth
+				}
+
+				if opts.IsPriorityHigh {
+					priority = ac.FCMPushPriorityHigh
+				} else {
+					priority = ac.FCMPushPriorityNormal
+				}
+
+				if opts.NotifierId != "" {
+					provider = ac.PushProvider{
+						NotifierId: opts.NotifierId,
+						FcmPushSettings: &ac.FCMConfig{
+							SupportAPNs: opts.SupportAPNs,
+						},
+					}
+				} else {
+					provider = ac.PushProvider{
+						Name:     opts.Name,
+						Provider: ac.PushProviderFCM,
+						Env:      ac.EnvProduct,
+						File:     opts.CertificateFilePath,
+						FcmPushSettings: &ac.FCMConfig{
+							PushType: pushType,
+							Priority: priority,
+							Version:  "v1",
+							Sound:    opts.Sound,
+						},
+					}
+				}
+			} else if opts.IsAPNs {
+				provider = ac.PushProvider{
+					Name:        opts.Name,
+					PackageName: opts.PackageName,
+					Provider:    ac.PushProviderAPNS,
+					Env:         env,
+					File:        opts.CertificateFilePath,
+					ApnsPushSettings: &ac.APNSConfig{
+						TeamId: opts.TeamID,
+						KeyId:  opts.KeyID,
+						Sound:  opts.Sound,
+					},
+				}
+
+			} else if opts.IsHuawei {
+				return fmt.Errorf("not support yet for Huawei")
+			} else {
+				return fmt.Errorf("unsupported push provider")
+			}
+			res, err := client.Provider().UpsertPushProvider(provider)
+			if err != nil {
+				logger.Error("failed to upsert push provider request,", map[string]interface{}{
+					"error": err,
+				})
+				return nil
+			}
+			logger.Info("Successfully upserted push provider", map[string]interface{}{
+				"uuid": res.Entities[0].UUID,
 			})
 			return nil
-		}
-		logger.Info("Push provider inserted successfully", map[string]interface{}{
-			"results": res.Entities,
-		})
-		return nil
-	},
+		},
+	}
+	fl := cmd.Flags()
+
+	// provider type
+	fl.BoolVarP(&opts.IsAPNs, "apns", "", false, "APNs")
+	fl.BoolVarP(&opts.IsFCM, "fcm", "", false, "FCM")
+	fl.BoolVarP(&opts.IsHuawei, "huawei", "", false, "Huawei")
+
+	// base config
+	fl.StringVarP(&opts.Name, "name", "n", "", "Name of the Certificate")
+	fl.BoolVarP(&opts.IsEnvDev, "dev-env", "e", false, "Set to true for DEVELOPMENT environment, false for PRODUCTION")
+	fl.StringVarP(&opts.Certificate, "cert", "c", "", "Certificate path")
+	fl.StringVarP(&opts.PackageName, "package-name", "p", "", "Package name")
+	fl.StringVarP(&opts.Sound, "sound", "s", "default", "Sound")
+
+	fl.StringVarP(&opts.CertificateFilePath, "file", "f", "", "Path of the certificate file")
+
+	fl.StringVarP(&opts.NotifierId, "notifier-id", "N", "", "Same as UUID for the push provider")
+
+	// *** FCM ***
+	// push message type
+	fl.BoolVarP(&opts.IsBoth, "both", "", false, "Both")
+	fl.BoolVarP(&opts.IsNotifacation, "noti", "", false, "Notification type")
+	fl.BoolVarP(&opts.IsData, "data", "", false, "Data type")
+
+	// Push Priority
+	fl.BoolVarP(&opts.IsPriorityHigh, "high", "", false, "Priority high")
+	fl.BoolVarP(&opts.IsPriorityNormal, "normal", "", false, "Priority normal")
+
+	fl.BoolVarP(&opts.SupportAPNs, "support-apns", "S", false, "Whether support APNs specific field (e.g. sound, mutable-content)")
+
+	// *** APNs ***
+	fl.StringVarP(&opts.TeamID, "team-id", "t", "", "Team ID of the APNs Certificate")
+	fl.StringVarP(&opts.KeyID, "key-id", "k", "", "Key ID of the APNs Certificate")
+
+	return cmd
 }
 
-var huaweiCmd = &cobra.Command{
-	Use:   "insert-huawei",
-	Short: "Insert a Huawei push provider",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		category, _ := cmd.Flags().GetString("category")
-		activityClass, _ := cmd.Flags().GetString("activityClass")
+// var huaweiCmd = &cobra.Command{
+// 	Use:   "insert-huawei",
+// 	Short: "Insert a Huawei push provider",
+// 	RunE: func(cmd *cobra.Command, args []string) error {
+// 		category, _ := cmd.Flags().GetString("category")
+// 		activityClass, _ := cmd.Flags().GetString("activityClass")
 
-		hw := getPushProviderFromCommonFlags(cmd)
-		hw.Type = ac.PushProviderFCM
-		hw.HuaweiPushSettings = &ac.HuaweiConfig{
-			Category:      category,
-			ActivityClass: activityClass,
-		}
+// 		hw := getPushProviderFromCommonFlags(cmd)
+// 		hw.Type = ac.PushProviderFCM
+// 		hw.HuaweiPushSettings = &ac.HuaweiConfig{
+// 			Category:      category,
+// 			ActivityClass: activityClass,
+// 		}
 
-		res, err := client.Provider().InsertPushProvider(hw)
-		if err != nil {
-			logger.Error("failed to send request,", map[string]interface{}{
-				"error": err,
-			})
-			return nil
-		}
-		logger.Info("Push provider inserted successfully", map[string]interface{}{
-			"results": res.Entities,
-		})
-		return nil
-	},
-}
+// 		res, err := client.Provider().InsertPushProvider(hw)
+// 		if err != nil {
+// 			logger.Error("failed to send request,", map[string]interface{}{
+// 				"error": err,
+// 			})
+// 			return nil
+// 		}
+// 		logger.Info("Push provider inserted successfully", map[string]interface{}{
+// 			"results": res.Entities,
+// 		})
+// 		return nil
+// 	},
+// }
 
 func init() {
 	pushCmd.AddCommand(providerCmd)
-	providerCmd.AddCommand(listProvidersCmd)
 
-	providerCmd.AddCommand(deleteProviderCmd)
-	deleteProviderCmd.Flags().StringP("uuid", "u", "", "the uuid of the provide")
+	providerCmd.AddCommand(listProvidersCmd)
+	providerCmd.AddCommand(upsertFcmCmd())
+	providerCmd.AddCommand(deleteProviderCmd())
 
 	// ANPS
-	providerCmd.AddCommand(apnsCmd)
-	addCommonFlags(apnsCmd)
+	// providerCmd.AddCommand(apnsCmd)
+	// addCommonFlags(apnsCmd)
 	// Provider-specific flags for APNS
-	apnsCmd.Flags().String("team-id", "", "team ID")
-	apnsCmd.MarkFlagRequired("team-id")
-	apnsCmd.Flags().String("key-id", "", "key ID")
-	apnsCmd.MarkFlagRequired("key-id")
+	// apnsCmd.Flags().String("team-id", "", "team ID")
+	// apnsCmd.MarkFlagRequired("team-id")
+	// apnsCmd.Flags().String("key-id", "", "key ID")
+	// apnsCmd.MarkFlagRequired("key-id")
 
 	//FCM
-	providerCmd.AddCommand(insertFcmCmd)
-	addCommonFlags(insertFcmCmd)
-	// Provider-specific flags for FCM
-	insertFcmCmd.Flags().String("push-type", "", "push type")
-	insertFcmCmd.Flags().String("priority", "", "priority")
-	insertFcmCmd.Flags().String("project-id", "", "project ID")
-	insertFcmCmd.Flags().String("version", "", "version")
+	// providerCmd.AddCommand(insertFcmCmd)
+	// addCommonFlags(insertFcmCmd)
+	// // Provider-specific flags for FCM
+	// insertFcmCmd.Flags().String("push-type", "", "push type")
+	// insertFcmCmd.Flags().String("priority", "", "priority")
+	// insertFcmCmd.Flags().String("project-id", "", "project ID")
+	// insertFcmCmd.Flags().String("version", "", "version")
 
-	providerCmd.AddCommand(huaweiCmd)
-	addCommonFlags(huaweiCmd)
+	// providerCmd.AddCommand(huaweiCmd)
+	// addCommonFlags(huaweiCmd)
 	// Provider-specific flags for HUAWEI
-	huaweiCmd.Flags().String("category", "", "category")
-	huaweiCmd.Flags().String("activityClass", "", "activityClass")
+	// huaweiCmd.Flags().String("category", "", "category")
+	// huaweiCmd.Flags().String("activityClass", "", "activityClass")
 
 }
 
-func addCommonFlags(cmd *cobra.Command) {
-	cmd.Flags().String("name", "", "Name of the Certificate")
-	cmd.Flags().String("env", "", "Environment (e.g., PRODUCTION, DEVELOPMENT)")
-	cmd.Flags().String("cert", "", "Certificate path")
-	cmd.Flags().String("pkg", "", "Package name")
-}
+// func addCommonFlags(cmd *cobra.Command) {
+// 	cmd.Flags().String("name", "", "Name of the Certificate")
+// 	cmd.Flags().String("env", "", "Environment (e.g., PRODUCTION, DEVELOPMENT)")
+// 	cmd.Flags().String("cert", "", "Certificate path")
+// 	cmd.Flags().String("pkg", "", "Package name")
+// }
 
-func getPushProviderFromCommonFlags(cmd *cobra.Command) ac.PushProvider {
-	name, _ := cmd.Flags().GetString("name")
-	env, _ := cmd.Flags().GetString("env")
+// func getPushProviderFromCommonFlags(cmd *cobra.Command) ac.PushProvider {
+// 	name, _ := cmd.Flags().GetString("name")
+// 	env, _ := cmd.Flags().GetString("env")
 
-	cert, _ := cmd.Flags().GetString("cert")
-	pkg, _ := cmd.Flags().GetString("pkg")
+// 	cert, _ := cmd.Flags().GetString("cert")
+// 	pkg, _ := cmd.Flags().GetString("pkg")
 
-	// Create the provider object based on the flags
-	return ac.PushProvider{
-		Name:        name,
-		Env:         env,
-		Certificate: cert,
-		PackageName: pkg,
-	}
-}
+// 	// Create the provider object based on the flags
+// 	return ac.PushProvider{
+// 		Name:        name,
+// 		Env:         env,
+// 		Certificate: cert,
+// 		PackageName: pkg,
+// 	}
+// }
--- http/payload.go
+++ http/payload.go
@@ -4,14 +4,17 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/url"
+	"os"
 	"strconv"
 
 	"howett.net/plist"
 )
 
 type Payload interface {
-	data() ([]byte, error)
+	data() ([]byte, string, error) // Returns data, content-type, and error
 }
 
 type XMLPayload struct {
@@ -26,31 +29,41 @@ type URLPayload struct {
 	Content map[string]interface{}
 }
 
-func (p *XMLPayload) data() ([]byte, error) {
+type FormPayload struct {
+	Fields map[string]interface{}
+	Files  map[string]string // map field names to file paths
+}
+
+func (p *XMLPayload) data() ([]byte, string, error) {
 	buffer := new(bytes.Buffer)
 
+	// Encode the content as XML
 	err := plist.NewEncoder(buffer).Encode(p.Content)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode plist: %w", err)
+		return nil, "", fmt.Errorf("failed to encode XML: %w", err)
 	}
 
-	return buffer.Bytes(), nil
+	// Return the XML data and content type
+	return buffer.Bytes(), "application/xml", nil
 }
 
-func (p *JSONPayload) data() ([]byte, error) {
+func (p *JSONPayload) data() ([]byte, string, error) {
 	buffer := new(bytes.Buffer)
 
+	// Encode the content as JSON
 	err := json.NewEncoder(buffer).Encode(p.Content)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode json: %w", err)
+		return nil, "", fmt.Errorf("failed to encode JSON: %w", err)
 	}
 
-	return buffer.Bytes(), nil
+	// Return the JSON data and content type
+	return buffer.Bytes(), "application/json", nil
 }
 
-func (p *URLPayload) data() ([]byte, error) {
+func (p *URLPayload) data() ([]byte, string, error) {
 	params := url.Values{}
 
+	// Encode key-value pairs as URL-encoded form
 	for key, val := range p.Content {
 		switch t := val.(type) {
 		case string:
@@ -58,9 +71,52 @@ func (p *URLPayload) data() ([]byte, error) {
 		case int:
 			params.Add(key, strconv.Itoa(val.(int)))
 		default:
-			return nil, fmt.Errorf("value type is not supported (%s)", t)
+			return nil, "", fmt.Errorf("value type is not supported (%s)", t)
 		}
 	}
 
-	return []byte(params.Encode()), nil
+	// Return the URL-encoded data and content type
+	return []byte(params.Encode()), "application/x-www-form-urlencoded", nil
+}
+
+func (p *FormPayload) data() ([]byte, string, error) {
+	body := new(bytes.Buffer)
+
+	// Create a new multipart writer with a boundary
+	writer := multipart.NewWriter(body)
+
+	// Add form fields
+	for key, val := range p.Fields {
+		err := writer.WriteField(key, fmt.Sprintf("%v", val))
+		if err != nil {
+			return nil, "", fmt.Errorf("failed to write field %s: %w", key, err)
+		}
+	}
+
+	// Add files to the form
+	for fieldName, filePath := range p.Files {
+		file, err := os.Open(filePath)
+		if err != nil {
+			return nil, "", fmt.Errorf("failed to open file %s: %w", filePath, err)
+		}
+		defer file.Close()
+
+		part, err := writer.CreateFormFile(fieldName, filePath)
+		if err != nil {
+			return nil, "", fmt.Errorf("failed to create form file %s: %w", fieldName, err)
+		}
+
+		_, err = io.Copy(part, file)
+		if err != nil {
+			return nil, "", fmt.Errorf("failed to copy file %s: %w", filePath, err)
+		}
+	}
+
+	err := writer.Close()
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to close writer: %w", err)
+	}
+
+	// Return the multipart form data and content type (including boundary)
+	return body.Bytes(), writer.FormDataContentType(), nil
 }
